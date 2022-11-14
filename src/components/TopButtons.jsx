a4066f0c85d99bd557a91161224d9b454b6af01c
Wilson Aballey <willywallet001@gmail.com>
2022-11-14 10:59:35 +0000

    First Commit

---
+++ src/components/TopButtons.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+function TopButtons() {
+
+    const cities =[
+        {
+            id:1,
+            title: 'Ghana'
+        },
+        {
+            id:2,
+            title: 'Lome'
+        }, {
+            id:3,
+            title: 'Lagos'
+        }, {
+            id: 5,
+            title: 'Tokyo'
+        },
+    ]
+  return (
+    <div className='flex items-center justify-around my-6'>
+        {
+        cities.map((city)=>(
+            <button key={city.id} className='text-white text-lg font-medium '>{city.title}</button>
+        ))
+        }
+    </div>
+  )
+}
+
+export default TopButtons
