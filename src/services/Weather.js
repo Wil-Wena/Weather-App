676756b303c315f095408b5351bf7f7ee079036f
Wilson Aballey <willywallet001@gmail.com>
2022-11-14 21:05:58 +0000

    Weather API call delay

---
+++ src/services/Weather.js
@@ -0,0 +1,7 @@
+const API_KEY ="e8c7152b492d98e40d86aa94b269f823";
+
+const BASE_URL = "https://api.openweathermap.org/data/2.5";
+
+const getWeatherData =(infoType, searchParams) =>{
+    const url = new URL(BASE_URL + infoType)
+}
